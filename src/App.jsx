c14a0a34a88adeba556b899e429720236aada3ce
Emilse Arias <emilsearias@gmail.com>
2024-09-17 15:08:23 -0300

    instalamos la libreria de react-router

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,18 @@ import Administrador from "./components/pages/Administrador";
 import FormularioProducto from "./components/pages/product/FormularioProducto";
 import Error404 from "./components/pages/Error404";
 import DetalleProducto from "./components/pages/DetalleProducto";
+import CardProducto from "./components/pages/product/CardProducto";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./App.css";
-import CardProducto from "./components/pages/product/CardProducto";
 
 function App() {
   return (
     <>
       <Menu></Menu>
-      <Index></Index>
+      {/* <Index></Index> */}
       {/* <Administrador></Administrador> */}
-      {/* <FormularioProducto></FormularioProducto> */}
+      <FormularioProducto></FormularioProducto>
       <Footer></Footer>
     </>
   );
