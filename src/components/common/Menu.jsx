e7b863f723cdbb056b55b3250cf9272f802b9a57
Emilse Arias <emilsearias@gmail.com>
2024-09-17 15:57:15 -0300

    agregar los links correspondientes del Menu

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,18 +1,20 @@
 import { Navbar, Container, Nav } from "react-bootstrap";
+import { Link, NavLink } from "react-router-dom";
+
 
 const Menu = () => {
   return (
     <Navbar expand="lg" className="bg-primary" data-bs-theme="dark">
       <Container>
-        <Navbar.Brand>
+        <Navbar.Brand as={Link} to='/'>
           <i className="bi bi-palette fs-3"></i> Libreria101
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link>Inicio</Nav.Link>
-            <Nav.Link>Administrador</Nav.Link>
-            <Nav.Link>Login</Nav.Link>
+            <NavLink end to='/' className='nav-link'>Inicio</NavLink>
+            <NavLink end to='/administrador' className='nav-link'>Administrador</NavLink>
+            <NavLink end to='/login' className='nav-link'>Login</NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
