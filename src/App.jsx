be11e33ca361badad61991c3e141a6cc90e39bb1
Emilse Arias <emilsearias@gmail.com>
2024-09-24 13:06:39 -0300

    agregue el componente login con su link en el navbar

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/pages/Login";
 
 function App() {
   return (
@@ -20,6 +21,10 @@ function App() {
       <Route path="/administrador" element={<Administrador></Administrador>}></Route>
       <Route path="/administrador/crear" element={<FormularioProducto></FormularioProducto>}></Route>
       <Route path="/administrador/editar" element={<FormularioProducto></FormularioProducto>}></Route>
+      <Route
+        exact path="/login"
+          element={<Login></Login>}
+        ></Route>
       <Route path="*" element={<Error404></Error404>}></Route>
     </Routes> 
     <Footer></Footer>
---
+++ src/components/pages/Login.jsx
@@ -0,0 +1,66 @@
+import { Form, Button, Container, Card } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+
+const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <Container className="mainSection">
+      <Card className="my-5">
+        <Card.Header as="h5">Login</Card.Header>
+        <Card.Body>
+          <Form onSubmit={handleSubmit(onSubmit)}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Ingrese un email"
+                {...register("email", {
+                    required: "El nombre de email es obligatorio",
+                    pattern: {
+                      value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/,
+                      message: "Ingrese una dirección de correo electrónico válida",
+                    }
+                  })}
+              />
+              <Form.Text className="text-danger">
+                {errors.email?.message}
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                {...register("password", {
+                    required: "El nombre de password es obligatorio",
+                    pattern: {
+                      value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/,
+                      message: "El password debe contener al menos una letra mayúscula, una letra minúscula y un número",
+                    },
+                  })}
+              />
+              <Form.Text className="text-danger">
+                {errors.password?.message}
+              </Form.Text>
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Ingresar
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
+};
+
+export default Login;
